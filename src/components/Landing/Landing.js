2081ac954c3adb87d262752f733967faca07b25b
zubae <zubae@live.in>
2021-04-09 01:32:31 +0530

    lading page done

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import { Button } from 'react-bootstrap';
 
 const Landing = () => {
     return (
@@ -8,11 +9,15 @@ const Landing = () => {
                 <Navbar />            
             </div>
             <div className='d-flex flex-row justify-content-center align-items-center page-height'>
-                <div>
-                    <h1>Video</h1>
+                <div className='video-div'>
+                    <video controls>
+                        <source src='/video/arch_vid.mp4' type='video/mp4' />
+                    </video>
                 </div>
-                <div>
-                    <h1>Text</h1>
+                <div className='text-div d-flex flex-column justify-content-around align-items-center ml-md-5'> 
+                    <h3>Welcome to</h3>
+                    <h1>Bluewalls</h1>
+                    <Button variant='secondary'>Get Started</Button>
                 </div>
             </div>
         </React.Fragment>
