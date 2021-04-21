4488317f1015c0cfcbe79304971f3e7d3c80e1b9
chan-chey <kchandran024@gmail.com>
2021-04-22 00:02:25 +0530

    second cj

--- src/components/Survey/SurveyResponse.js
+++ src/components/Survey/SurveyResponse.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const SurveyResponse = () => {
     return (
-        <div className='response-bg'>
+        <div className='response-bg' style={{ overflow: "hidden" }}>
             <div className='container'>
                 <Link to='/'>
                     <div className='brand brand-light pt-3'>
@@ -12,8 +12,8 @@ const SurveyResponse = () => {
                 </Link>
             </div>
             <div className='d-flex flex-column justify-content-center align-items-center h-100'>
-                <div className='survey-block p-3 text-center' style={{width: '30vw', height: '20vh'}}>
-                    <p>Thank you for taking time to fill the form, we will contact you with an irresistible offer!!</p>
+                <div className='survey-block p-3 text-center ' >
+                    <span>Thank you for taking time to fill the form, we will contact you with an irresistible offer!!</span>
                 </div>
             </div>
         </div>
