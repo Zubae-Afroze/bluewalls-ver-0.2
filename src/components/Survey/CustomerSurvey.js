2d977b06c543fc409df214c0dea9aec6ed429cd2
zubae <afroze@outlook.in>
2021-04-21 19:11:42 +0530

    survey form chagnes

--- src/components/Survey/CustomerSurvey.js
+++ src/components/Survey/CustomerSurvey.js
@@ -52,26 +52,26 @@ class CustomerSurvey extends Component {
                             <h1>Bluewalls</h1>
                         </div>
                     </Link>
-                    <div className='d-flex justify-content-center mt-4'>
+                    <div className='d-flex justify-content-center survey-wrap'>
                         <div className='survey-block pt-3'>
                             <div className='back-btn ml-3'><Link to='/presurvey'><i className="fas fa-chevron-left"></i>Go Back</Link></div>
                             <div className='d-flex justify-content-center '>
                                 <Form onSubmit={this.submitHandler}>
-                                    <Form.Group className="d-flex">
+                                    <Form.Group className="d-flex justify">
                                         <Form.Label className='mr-2 pt-1'>Location</Form.Label>
                                         <Form.Control className='w-50' type='text' id='location' onChange={this.changeHandler} placeholder='Answer' />
                                         <Form.Label className='mx-2 pt-1'>Circle</Form.Label>
                                         <Form.Control type='text' className='w-50' id='circle' onChange={this.changeHandler} placeholder='Kms' />
                                     </Form.Group>
                                     <Form.Group className="d-flex">
-                                        <Form.Label className='mr-3 pt-1'>Rooms</Form.Label>
+                                        <Form.Label className='mr-3 pt-1'>Rooms&#160;</Form.Label>
                                         <Form.Control className='w-50' type='text' id='location' onChange={this.changeHandler} placeholder='Answer' />
                                         <Form.Label className='mx-2 pt-1'>Area</Form.Label>
                                         <Form.Control type='text' className='w-50' id='circle' onChange={this.changeHandler} placeholder='In m2' />
                                     </Form.Group>
                                     <Form.Group id="apartmentType">
                                         <Form.Label>Apartment Type</Form.Label>
-                                        <Form.Group className='d-flex flex-wrap justify-content-start'>
+                                        <Form.Group className='d-flex flex-wrap justify-content-between'>
                                             <Form.Check type="checkbox" label="Basement" className='m-2' />
                                             <Form.Check type="checkbox" label="Ground Floor" className='m-2' />
                                             <Form.Check type="checkbox" label="Mezzanine Floor" className='m-2' />
