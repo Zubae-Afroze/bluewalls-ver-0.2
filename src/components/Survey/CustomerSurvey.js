a071940bb4416fd8b14ce18ac782d28d278a5698
zubae <afroze@outlook.in>
2021-04-23 15:31:16 +0530

    form to database done

--- src/components/Survey/CustomerSurvey.js
+++ src/components/Survey/CustomerSurvey.js
@@ -13,13 +13,23 @@ class CustomerSurvey extends Component {
             circle: '',
             rooms: '',
             area: '',
-            apartmentType: {
-
-            },
+            basement: false,
+            goundFloor: false,
+            mezzanineFloor: false,
+            flat: false,
+            loft: false,
+            masionette: false,
+            terraceApartment: false,
+            penthouse: false,
+            attic: false,
+            other: false,
             floorPreference: '',
-            furnishing: {
-
-            }
+            garage: false,
+            equipedKitchen: false,
+            passengerElevator: false,
+            cellar: false,
+            guestToilet: false,
+            surveyType: 'customer'
         }
     }
 
@@ -27,7 +37,15 @@ class CustomerSurvey extends Component {
 
     changeHandler = (e) => {
         this.setState({
-            [e.target.id]: e.target.value
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    checkHandler = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.checked
         })
     }
 
@@ -36,6 +54,7 @@ class CustomerSurvey extends Component {
         e.preventDefault();
         this.props.customerSurveySubmit(this.state);
         this.props.history.push('/surveyresponse');
+        console.log(this.state);
     }
 
 
@@ -59,43 +78,43 @@ class CustomerSurvey extends Component {
                                 <Form onSubmit={this.submitHandler}>
                                     <Form.Group className="d-flex justify">
                                         <Form.Label className='mr-2 pt-1' style={{ width: "60px" }}>Location</Form.Label>
-                                        <Form.Control className='w-50' type='text' id='location' onChange={this.changeHandler} placeholder='Answer' />
+                                        <Form.Control className='w-50' type='text' name='location' onChange={this.changeHandler} placeholder='Answer' />
                                         <Form.Label className='mx-2 pt-1' style={{ width: "60px" }}>Circle</Form.Label>
-                                        <Form.Control type='text' className='w-50' id='circle' onChange={this.changeHandler} placeholder='Kms' />
+                                        <Form.Control type='text' className='w-50' name='circle' onChange={this.changeHandler} placeholder='Kms' />
                                     </Form.Group>
                                     <Form.Group className="d-flex">
                                         <Form.Label className='mr-20px pt-1' style={{ width: "60px" }}>Rooms</Form.Label>
-                                        <Form.Control className='w-50' type='text' id='location' onChange={this.changeHandler} placeholder='Answer' />
+                                        <Form.Control className='w-50' type='text' name='rooms' onChange={this.changeHandler} placeholder='Answer' />
                                         <Form.Label className='mx-2 pt-1' style={{ width: "60px" }}>Area</Form.Label>
-                                        <Form.Control type='text' className='w-50' id='circle' onChange={this.changeHandler} placeholder='In m2' />
+                                        <Form.Control type='text' className='w-50' name='area' onChange={this.changeHandler} placeholder='In m2' />
                                     </Form.Group>
                                     <Form.Group id="apartmentType">
                                         <Form.Label>Apartment Type</Form.Label>
                                         <Form.Group className='d-flex flex-wrap justify-content-between'>
-                                            <Form.Check type="checkbox" label="Basement" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Ground Floor" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Mezzanine Floor" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Flat" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Loft" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Maisonette" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Terrace Apartment" className='m-2' />
-                                            <Form.Check type="checkbox" label="Penthouse" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Attic" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Other" className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Basement" name="basement" value={this.basement} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Ground Floor" name="goundFloor" value={this.groundFloor} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Mezzanine Floor" name="mezzanineFloor" value={this.mezzanineFloor} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Flat" name="flat" value={this.flat} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Loft" name="loft" value={this.loft} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Maisonette" name="maisonette" value={this.maisonette} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Terrace Apartment" name='terraceAppartment' value={this.terraceApartment} onChange={this.checkHandler} className='m-2' />
+                                            <Form.Check type="checkbox" label="Penthouse" name="penthouse" value={this.penthouse} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Attic" name="attic" value={this.attic} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Other" name="other" value={this.other} onChange={this.checkHandler} className='m-2 w-150px' />
                                         </Form.Group>
                                     </Form.Group>
                                     <Form.Group>
                                         <Form.Label>Floor Preference</Form.Label>
-                                        <Form.Control type='text' id='question3' onChange={this.changeHandler} placeholder='Answer' />
+                                        <Form.Control type='text' name='floorPreference' onChange={this.changeHandler} placeholder='Answer' />
                                     </Form.Group>
                                     <Form.Group id="question4">
                                         <Form.Label>Furnishing</Form.Label>
                                         <Form.Group className='d-flex flex-wrap justify-content-between'>
-                                            <Form.Check type="checkbox" label="Garage /Parking Space" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Equipped Kitchen" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Passenger Elevator" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Basement,Cellar" className='m-2 w-150px' />
-                                            <Form.Check type="checkbox" label="Guest Toilet" className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Garage /Parking Space" name='garage' value={this.garage} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Equipped Kitchen" name='equipedKitchen' value={this.equipedKitchen} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Passenger Elevator" name='passengerElevator' value={this.passengerElevator} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Basement / Cellar" name='cellar' value={this.cellar} onChange={this.checkHandler} className='m-2 w-150px' />
+                                            <Form.Check type="checkbox" label="Guest Toilet" name='guestToilet' value={this.guestToilet} onChange={this.checkHandler} className='m-2 w-150px' />
                                         </Form.Group>
                                     </Form.Group>
                                     <Button variant='secondary' type='submit'>Submit</Button>
