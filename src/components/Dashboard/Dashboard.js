22389cee44df9c3fefb9d3b6762342d8acfc9408
chan-chey <kchandran024@gmail.com>
2021-04-25 17:10:58 +0530

    dashboard responsiveness

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,43 +10,43 @@ const Dashboard = (props) => {
     return (
         <div>
             <div className='dashboard-bg'>
-                    <div className='brand brand-light pt-3 ml-3'>
-                        <Link to='/'>
-                            <img src='/bluelogoinvert.png' alt='bluewalls-logo' className='bluelogo' />
-                        </Link>
-                    </div>
-                    <div className='dashboard-main'>
-                        <div className='dashboard-survey'>
-                            <h1>My Surveys</h1>
-                            <div>
-                                <div className='dash-card'>
+                <div className='brand brand-light pt-3 ml-3'>
+                    <Link to='/'>
+                        <img src='/bluelogoinvert.png' alt='bluewalls-logo' className='bluelogo' />
+                    </Link>
+                </div>
+                <div className='dashboard-main d-md-flex'>
+                    <div className='dashboard-survey col-10 col-md-5 ml-4 ml-md-0'>
+                        <h1>My Surveys</h1>
+                        <div>
+                            <div className='dash-card '>
                                 {
                                     props.survey ?
-                                    props.survey.map(obj => (
-                                        <Card  key={obj.id}>
-                                            Survey Type: {obj.surveyType}<br />
-                                            Location : {obj.location} <br/>
-                                            Circle: {obj.circle} <br/>
-                                            Rooms: {obj.rooms} <br/>
-                                            Area: {obj.area} <br/>
+                                        props.survey.map(obj => (
+                                            <Card key={obj.id} className='col-md-6 col-10 '>
+                                                Survey Type: {obj.surveyType}<br />
+                                            Location : {obj.location} <br />
+                                            Circle: {obj.circle} <br />
+                                            Rooms: {obj.rooms} <br />
+                                            Area: {obj.area} <br />
                                             Selected Ammenities: {obj.checked.map((e, index) => (<span className='select' key={index}>{e}</span>))}
-                                        </Card>
-                                    )) : null
+                                            </Card>
+                                        )) : null
                                 }
-                                </div>
-                                <div className='add-icon'>
-                                    <Link to='/presurvey'>
-                                        <i className="fas fa-plus-circle"></i>
-                                    </Link>
-                                </div>
+                            </div>
+                            <div className='add-icon'>
+                                <Link to='/presurvey'>
+                                    <i className="fas fa-plus-circle"></i>
+                                </Link>
                             </div>
                         </div>
-                        <div className='dashboard-profile'>
-                            <h1>My Profile</h1>
-                            <div>Name: {props.profile.fname + ' ' + props.profile.lname}</div>
-                            <div>Registered Email: {props.auth.email}</div>
-                        </div>
                     </div>
+                    <div className='dashboard-profile col-10 col-md-5 ml-4 ml-md-0 mt-3 mt-md-0'>
+                        <h1>My Profile</h1>
+                        <div>Name: {props.profile.fname + ' ' + props.profile.lname}</div>
+                        <div>Registered Email: {props.auth.email}</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
